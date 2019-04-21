1464aad1f29bbacba02fa68cd95ca219b8e468e4
stephen <jonesstephen239@gmail.com>
2019-04-21 12:58:46 -0500

    fixed formating for route explorer

--- client/src/components/visual/route.js
+++ client/src/components/visual/route.js
@@ -54,7 +54,7 @@ class Route extends Component {
     }
 
     handleChange = (event, value) => {
-        this.setState({ value });
+        this.setState({value});
     };
 
     async componentDidMount() {
@@ -100,170 +100,175 @@ class Route extends Component {
 
 
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
         return (
-            <div >
-                <Typography variant="h5" gutterBottom style={{color:'red'}}>
+            <div>
+                <Typography variant="h5" gutterBottom style={{color: 'red'}}>
                     Currently Exploring: {this.props.iata}
                 </Typography>
-            <div style={{display: 'flex', position: 'fixed', width: '100%', height: '100%'}}>
+                <div style={{display: 'flex', position: 'fixed', width: '100%', height: '100%'}}>
 
-                <div id="map">
+                    <div id="map">
 
-                    {this.state.traffic.length === 0 ?
-                        <RingLoader
-                            css={override}
-                            sizeUnit={"px"}
-                            size={'300'}
-                            color={'#123abc'}
-                            loading={true}
-                        />
-                        :
-                        <Map center={[40, -95]} zoom={4}>
-                            <HeatmapLayer
-                                fitBoundsOnLoad
-                                fitBoundsOnUpdate
-                                points={this.state.traffic}
-                                longitudeExtractor={m => {
-                                    let airport = this.state.airports[m['airport']];
-                                    return airport === undefined ? null : airport['longitude'];
-                                }}
-                                latitudeExtractor={m => {
-                                    let airport = this.state.airports[m['airport']];
-                                    return airport === undefined ? null : airport['latitude'];
-                                }}
-                                intensityExtractor={m => parseFloat(m['intensity'])}/>
-                            <TileLayer
-                                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            />
-
-                            <Control position="topright" >
-                                <Button color="primary" variant="contained"
-                                        onClick={ () => alert("YES") }
-                                >
-                                    Performance
-                                </Button>
-                                <Button color="secondary" variant="contained"
-                                        onClick={ () => alert("YES") }
-                                >
-                                    Traffic
-                                </Button>
-                                <Button color="primary" variant="contained"
-                                        onClick={ () => alert("YES") }
-                                >
-                                    Routes
-                                </Button>
-                            </Control>
-
-                        </Map>
-                    }
-                </div>
-
-                <div id="predictor">
-
-                    <div style={{textAlign: "center"}}>
-                        <Typography variant="h3" gutterBottom>
-                           Route Explorer
-                        </Typography>
-                        <Typography variant="subtitle1" gutterBottom>
-                            Find the best airline for a specific trip based on your preferences!
-                        </Typography>
-                        <form className={classes.container} noValidate autoComplete="off">
-                            <TextField
-                                id="outlined-name"
-                                label="Origin"
-                                className={classes.textField}
-                                margin="normal"
-                                variant="outlined"
-                            />
-                            <TextField
-                                id="outlined-uncontrolled"
-                                label="Destination"
-                                className={classes.textField}
-                                margin="normal"
-                                variant="outlined"
-                            />
-                        </form>
-                        <Button variant="contained" color="primary" onClick={this.find}>
-                            Explore
-                        </Button>
-                    </div>
-                    <div style={{width: '50%', height: '50%'}}>
-                        {this.state.keys.length !== 0 ?
-                            <ResponsiveChordCanvas
-                                matrix={this.state.matrix.slice(0,100)}
-                                keys={this.state.keys.slice(0,100)}
-                                margin={{
-                                    "top": 60,
-                                    "right": 60,
-                                    "bottom": 60,
-                                    "left": 60
-                                }}
-                                pixelRatio={1}
-                                padAngle={0.006}
-                                innerRadiusRatio={0.86}
-                                innerRadiusOffset={0}
-                                arcOpacity={1}
-                                arcBorderWidth={1}
-                                arcBorderColor={{
-                                    "from": "color",
-                                    "modifiers": [
-                                        [
-                                            "darker",
-                                            0.4
-                                        ]
-                                    ]
-                                }}
-                                ribbonOpacity={0.8}
-                                ribbonBorderWidth={10}
-                                ribbonBorderColor={{
-                                    "from": "color",
-                                    "modifiers": [
-                                        [
-                                            "darker",
-                                            0.6
-                                        ]
-                                    ]
-                                }}
-                                enableLabel={true}
-                                label="id"
-                                labelOffset={9}
-                                labelRotation={-90}
-                                labelTextColor={{
-                                    "from": "color",
-                                    "modifiers": [
-                                        [
-                                            "darker",
-                                            1
-                                        ]
-                                    ]
-                                }}
-                                colors={{
-                                    "scheme": "paired"
-                                }}
-                                isInteractive={true}
-                                arcHoverOpacity={1}
-                                arcHoverOthersOpacity={0.4}
-                                ribbonHoverOpacity={1}
-                                ribbonHoverOthersOpacity={0}
-                                animate={true}
-                                motionStiffness={90}
-                                motionDamping={7}
-                            /> : <RingLoader
+                        {this.state.traffic.length === 0 ?
+                            <RingLoader
                                 css={override}
                                 sizeUnit={"px"}
                                 size={'300'}
                                 color={'#123abc'}
                                 loading={true}
+                            />
+                            :
+                            <Map center={[40, -95]} zoom={4}>
+                                <HeatmapLayer
+                                    fitBoundsOnLoad
+                                    fitBoundsOnUpdate
+                                    points={this.state.traffic}
+                                    longitudeExtractor={m => {
+                                        let airport = this.state.airports[m['airport']];
+                                        return airport === undefined ? null : airport['longitude'];
+                                    }}
+                                    latitudeExtractor={m => {
+                                        let airport = this.state.airports[m['airport']];
+                                        return airport === undefined ? null : airport['latitude'];
+                                    }}
+                                    intensityExtractor={m => parseFloat(m['intensity'])}/>
+                                <TileLayer
+                                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                                 />
+
+                                <Control position="topright">
+                                    <Button color="primary" variant="contained"
+                                            onClick={() => alert("YES")}
+                                    >
+                                        Performance
+                                    </Button>
+                                    <Button color="secondary" variant="contained"
+                                            onClick={() => alert("YES")}
+                                    >
+                                        Traffic
+                                    </Button>
+                                    <Button color="primary" variant="contained"
+                                            onClick={() => alert("YES")}
+                                    >
+                                        Routes
+                                    </Button>
+                                </Control>
+
+                            </Map>
                         }
                     </div>
 
+                    <div id="predictor">
+
+                        <div style={{textAlign: "center"}}>
+                            <Typography variant="h3" gutterBottom>
+                                Route Explorer
+                            </Typography>
+                            <Typography variant="subtitle1" gutterBottom>
+                                Find the best airline for a specific trip based on your preferences!
+                            </Typography>
+                            <form className={classes.container} noValidate autoComplete="off">
+                                <TextField
+                                    id="outlined-name"
+                                    label="Origin"
+                                    className={classes.textField}
+                                    margin="normal"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    id="outlined-uncontrolled"
+                                    label="Destination"
+                                    className={classes.textField}
+                                    margin="normal"
+                                    variant="outlined"
+                                />
+                            </form>
+                            <Button variant="contained" color="primary" onClick={this.find}>
+                                Explore
+                            </Button>
+                        </div>
+
+                        <div id="results" style={{display: 'flex', flexDirection: 'row', height: '100%'}}>
+                            <div style={{ backgroundColor: 'red', width: '40%', height: '100%'}}>
+                            </div>
+                            <div style={{width: '60%', height: '100%'}}>
+                                {this.state.keys.length !== 0 ?
+                                    <ResponsiveChordCanvas
+                                        matrix={this.state.matrix.slice(0, 100)}
+                                        keys={this.state.keys.slice(0, 100)}
+                                        margin={{
+                                            "top": 60,
+                                            "right": 60,
+                                            "bottom": 200,
+                                            "left": 60
+                                        }}
+                                        pixelRatio={1}
+                                        padAngle={0.006}
+                                        innerRadiusRatio={0.86}
+                                        innerRadiusOffset={0}
+                                        arcOpacity={1}
+                                        arcBorderWidth={1}
+                                        arcBorderColor={{
+                                            "from": "color",
+                                            "modifiers": [
+                                                [
+                                                    "darker",
+                                                    0.4
+                                                ]
+                                            ]
+                                        }}
+                                        ribbonOpacity={0.8}
+                                        ribbonBorderWidth={10}
+                                        ribbonBorderColor={{
+                                            "from": "color",
+                                            "modifiers": [
+                                                [
+                                                    "darker",
+                                                    0.6
+                                                ]
+                                            ]
+                                        }}
+                                        enableLabel={true}
+                                        label="id"
+                                        labelOffset={9}
+                                        labelRotation={-90}
+                                        labelTextColor={{
+                                            "from": "color",
+                                            "modifiers": [
+                                                [
+                                                    "darker",
+                                                    1
+                                                ]
+                                            ]
+                                        }}
+                                        colors={{
+                                            "scheme": "paired"
+                                        }}
+                                        isInteractive={true}
+                                        arcHoverOpacity={1}
+                                        arcHoverOthersOpacity={0.4}
+                                        ribbonHoverOpacity={1}
+                                        ribbonHoverOthersOpacity={0}
+                                        animate={true}
+                                        motionStiffness={90}
+                                        motionDamping={7}
+                                    /> : <RingLoader
+                                        css={override}
+                                        sizeUnit={"px"}
+                                        size={'300'}
+                                        color={'#123abc'}
+                                        loading={true}
+                                    />
+                                }
+                            </div>
+                        </div>
+
+                    </div>
+
+
                 </div>
-
-
-            </div>
             </div>)
     }
 }
