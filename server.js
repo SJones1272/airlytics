2625fc7991e1287753db8ca3a69e011d5dbaa742
stephen <jonesstephen239@gmail.com>
2019-04-21 02:31:57 -0500

    I am dumb...

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
-// Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
-
 // app.use('/api/financial', require('routes/financial'));
 // app.use('/api/airline', require('./routes/ranking'));
 app.use('/api/performance', require('./routes/performance/index'));
 app.use('/api/routes', require('./routes/routes'));
 app.use('/api/airports', require('./routes/airports'));
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port, () => {
